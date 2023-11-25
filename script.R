f23d429a7554fd056990882e16bd0ff7b25ac12a
severin.clauss <severin.clauss@gmail.com>
2023-11-25 18:09:28 +0100

    first steps

--- script.R
+++ script.R
@@ -2,27 +2,27 @@ library(readr)
 library(dlookr)
 
 # load dataset for later analysis
-data_loans <- read_csv("./loan_sample_7.csv")
-
-data <- data_loans # We always make a copy from the original dataset and work on the copy
+data_improt <- read_csv('./loan_sample_7.csv')
+loan_data <- data_improt
 
 # ANALYZE DATA QUALITY
 # we start with checking the dimension, structure, head and tail of our dataset
-dim(data) # this shows us the demension of our dataset (40'000 Rows, 17 Cols)
-str(data) # structure of the dataset and its variables
-head(data)
-tail(data)
+dim(loan_data)
+str(loan_data)
+head(loan_data)
+tail(loan_data)
 
 # We check the summary of all variables included.
-summary(data)
+summary(loan_data)
 
-# we check the overview from dlookr to get a better understanding of the data quality
-overview(data)
-overview <- overview(data)
+# we check the overview from dlookr to get a better understanding of the loan_data quality
+overview(loan_data)
+overview <- overview(loan_data)
 plot(overview)
 
+
 # we check for missing values
-diagnose(data, 1:17) # diagnose shows us, that there are no missing values so far
+diagnose(loan_data, 1:17) # diagnose shows us, that there are no missing values so far
 
 
 #####################
