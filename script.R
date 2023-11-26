b27d97861a8928e391034395179b01cb9d3b26b3
severin.clauss <severin.clauss@gmail.com>
2023-11-26 13:27:19 +0100

    answer Summarize variables section

--- script.R
+++ script.R
@@ -6,15 +6,15 @@ library(dplyr)
 data_improt <- read_csv('./loan_sample_7.csv')
 loan_data <- data_improt
 
-# ANALYZE DATA QUALITY
+#########
+# Structure
+# Check and report the structure of the data set.
+
 # we start with checking the dimension, structure, head and tail of our dataset
 dim(loan_data)
 str(loan_data)
 head(loan_data)
 tail(loan_data)
-
-# We check the summary of all variables included.
-summary(loan_data)
 
 # we check the overview from dlookr to get a better understanding of the loan_data quality
 overview(loan_data)
@@ -23,6 +23,8 @@ plot(overview)
 
 
 #########
+# Categorical variables
+
 # How many numeric and how many categorical variables are included in the data?
 # What categorical variable has the most levels in it?
 
@@ -36,44 +38,8 @@ class_variables <- c('grade', 'home_ownership', 'verification_status', 'purpose'
 
 result <- lapply(class_variables, function(x) loan_data |> distinct(across({{x}})))
 
+#########
+# Summarize variables
+# Summarize the variables. Discuss the summary statistics obtained.
 
-grade <- loan_data |>
-  distinct(grade)
-
-home_ownership <- loan_data |>
-  distinct(home_ownership)
-
-verification_status <- loan_data |>
-  distinct(verification_status)
-
-purpose <- loan_data |>
-  distinct(purpose)
-
-application_type <- loan_data |>
-  distinct(application_type)
-
-
-
-
-
-grade <- loan_data |>
-  distinct(grade) |>
-  count()
-
-home_ownership <- loan_data |>
-  distinct(home_ownership) |>
-  count()
-
-verification_status <- loan_data |>
-  distinct(verification_status) |>
-  count()
-
-purpose <- loan_data |>
-  distinct(purpose) |>
-  count()
-
-application_type <- loan_data |>
-  distinct(application_type) |>
-  count()
-
-
+summary(loan_data)
