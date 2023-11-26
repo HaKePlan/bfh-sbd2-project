d64df2e293ed475903d3dc37137cef8f8c3b4159
severin.clauss <severin.clauss@gmail.com>
2023-11-26 22:30:43 +0100

    answer Checking for outliers section

--- script.R
+++ script.R
@@ -2,6 +2,8 @@ library(readr)
 library(dlookr)
 library(dplyr)
 library(ggplot2)
+library(tidyr)
+library(purrr)
 
 # load dataset for later analysis
 data_improt <- read_csv('./loan_sample_7.csv')
@@ -58,4 +60,59 @@ ggplot(loan_data, aes(x = Status, fill = Status)) +
   ylab("Count") +
   xlab("Status of the loan") +
   scale_fill_manual(values = c('skyblue', 'brown2'), labels = c("0 (Not Defaulted)", "1 (Defaulted)"))
+
+#########
+# Distribution of numeric variables
+# Check the distribution of the numeric variables in the data set (include different visual representations).
+
+numeric_vars <- loan_data |> select_if(is.numeric)
+
+# Gather the data for plotting
+gathered_data <- gather(numeric_vars, key = "Variable", value = "Value")
+
+# Plot the distribution of numeric variables
+ggplot(gathered_data, aes(x = Value, fill = Variable)) +
+  geom_histogram(show.legend = FALSE) +
+  facet_wrap(~ Variable, scales = "free") +
+  labs(title = "Distribution of Numeric Variables",
+       x = "",
+       y = "") +
+  theme_minimal()
+
+#########
+# Checking for outliers
+# Investigate whether certain variables contain outliers (hint: what does a box plot show?).
+
+ggplot(gathered_data, aes(x = Value, fill = Variable)) +
+  geom_boxplot() +
+  facet_wrap(~ Variable, scales = "free") +
+  labs(title = "Distribution of Numeric Variables",
+       x = "",
+       y = "") +
+  theme_minimal()
+
+boxplot(scale(numeric_vars[,1:11]), use.cols = TRUE)
+
+diagnose_outlier(numeric_vars)
+
+numeric_vars |> plot_outlier(diagnose_outlier(numeric_vars) |> filter(outliers_ratio >= 0.5) |> select(variables) |> unlist())
+
+
+#########
+# Dealing with outliers
+# Elaborate your view on how to proceed in dealing with the outliers and – if necessary, take appropriate action.
+
+# TODO investigate in how to deal with outliers (week 5)
+# outlier <- function(x) {
+#   quantiles <- quantile(x, c(.05, .95))
+#   x[x < quantiles[1]] <- quantiles[1]
+#   x[x > quantiles[2]] <- quantiles[2]
+#   x
+# }
+#
+# data_new_numeric <- map_df(numeric_vars[,-c(20:24)], outlier)
+# cols <- numeric_vars[,c(20:24)]
+# data_new_numeric <- cbind(data_new_numeric, cols)
+#
+# boxplot(scale(data_new_numeric[,c(1:19)]), use.cols = TRUE)
 
