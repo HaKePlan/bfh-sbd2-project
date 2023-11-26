6231d84aea244afea5b14be0a8c259a4e7028a89
severin.clauss <severin.clauss@gmail.com>
2023-11-26 13:11:57 +0100

    answer Categorical variables section

--- script.R
+++ script.R
@@ -1,5 +1,6 @@
 library(readr)
 library(dlookr)
+library(dplyr)
 
 # load dataset for later analysis
 data_improt <- read_csv('./loan_sample_7.csv')
@@ -21,12 +22,58 @@ overview <- overview(loan_data)
 plot(overview)
 
 
-# we check for missing values
-diagnose(loan_data, 1:17) # diagnose shows us, that there are no missing values so far
+#########
+# How many numeric and how many categorical variables are included in the data?
+# What categorical variable has the most levels in it?
+
+overview |>
+  filter(division == 'data type' & value > 0) |>
+  select(metrics, value)
+
+# from plot(overview) we know which are character variables
+# lets check if each character variable is also a classifier
+class_variables <- c('grade', 'home_ownership', 'verification_status', 'purpose', 'application_type')
+
+result <- lapply(class_variables, function(x) loan_data |> distinct(across({{x}})))
 
 
-#####################
-# Exercise 1
-#
-#
-#####################
+grade <- loan_data |>
+  distinct(grade)
+
+home_ownership <- loan_data |>
+  distinct(home_ownership)
+
+verification_status <- loan_data |>
+  distinct(verification_status)
+
+purpose <- loan_data |>
+  distinct(purpose)
+
+application_type <- loan_data |>
+  distinct(application_type)
+
+
+
+
+
+grade <- loan_data |>
+  distinct(grade) |>
+  count()
+
+home_ownership <- loan_data |>
+  distinct(home_ownership) |>
+  count()
+
+verification_status <- loan_data |>
+  distinct(verification_status) |>
+  count()
+
+purpose <- loan_data |>
+  distinct(purpose) |>
+  count()
+
+application_type <- loan_data |>
+  distinct(application_type) |>
+  count()
+
+
